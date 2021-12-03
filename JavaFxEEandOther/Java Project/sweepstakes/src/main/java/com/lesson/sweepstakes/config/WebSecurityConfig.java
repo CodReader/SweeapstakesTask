3f0e834c984607ab0f26447f6275e980b0ac45e0
bogdan <onevariant7@gmai.com>
2021-12-03 15:34:58 +0200

    Controller home

--- JavaFxEEandOther/Java Project/sweepstakes/src/main/java/com/lesson/sweepstakes/config/WebSecurityConfig.java
+++ JavaFxEEandOther/Java Project/sweepstakes/src/main/java/com/lesson/sweepstakes/config/WebSecurityConfig.java
@@ -2,13 +2,11 @@ package com.lesson.sweepstakes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
@@ -16,18 +14,23 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity(debug = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure (HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/authenticated/**").authenticated()
-                .and()
+                .antMatchers("/home")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                    .and()
                 .formLogin()
-                .and()
-                .logout().logoutSuccessUrl("/");
+                    .and()
+                .logout()
+                    .logoutSuccessUrl("/home");
     }
 
-@Bean
-    public JdbcUserDetailsManager users (DataSource dataSource){
+    @Bean
+    public JdbcUserDetailsManager users (DataSource dataSource) {
 
         UserDetails user = User.builder()
                 .username("user")
@@ -37,33 +40,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        if( jdbcUserDetailsManager.userExists(user.getUsername()))
-        {
-            jdbcUserDetailsManager.deleteUser(user.getUsername());
+        if (!jdbcUserDetailsManager.userExists(user.getUsername())) {
+            jdbcUserDetailsManager.createUser(user);
         }
-        jdbcUserDetailsManager.createUser(user);
-
-
 
         return jdbcUserDetailsManager;
-
     }
-
 }
-  /*@Bean
-    public JdbcUserDetailsManager users(DataSource dataSource){
-        UserDetails = User.builder()
-        .username("user");
-        .password("(bcrypt)$2a$12$8JjPBYDOsshi.hViMc1CkO/9wCvyo/OHwlP9e3b8YBkNwp18v6Unm")
-                .build();
-
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        if( jdbcUserDetailsManager.userExists(user.getUsername()))
-        {
-            jdbcUserDetailsManager.deleteUser(user.getUsername());
-        }
-
-        jdbcUserDetailsManager.createUser(user);
-        return null;*/
-// jdbcauth
--- JavaFxEEandOther/Java Project/sweepstakes/src/main/java/com/lesson/sweepstakes/controller/TestController.java
+++ JavaFxEEandOther/Java Project/sweepstakes/src/main/java/com/lesson/sweepstakes/controller/TestController.java
@@ -1,25 +1,21 @@
 package com.lesson.sweepstakes.controller;
 
-/*import com.lesson.sweepstakes.dao.User;*/
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
-@Controller
 @RestController
-public class MainController {
+@RequestMapping(value = "/test")
+public class TestController {
 
-    @GetMapping("/")
-    public String homePage()
-    {
-       return "home";
-   }
-
-    @GetMapping("/authenticated")
+    @GetMapping
     public String pageForAuthenticatedUsers(){
+        return "secured part of web" ;
+    }
+
+    @PostMapping
+    public String postpageForAuthenticatedUsers(){
         return "secured part of web" ;
     }
 }
