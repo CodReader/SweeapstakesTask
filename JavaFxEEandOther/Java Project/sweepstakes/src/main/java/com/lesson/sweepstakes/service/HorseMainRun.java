f30fcd2fcfb5adfddfce07c078dcb69ac397981b
bogdan <onevariant7@gmai.com>
2022-01-21 16:59:11 +0200

    Changed the Maine class also in the race began to figure out how to return one horse and also the result of the race.

--- JavaFxEEandOther/Java Project/sweepstakes/src/main/java/com/lesson/sweepstakes/service/HorseMainRun.java
+++ JavaFxEEandOther/Java Project/sweepstakes/src/main/java/com/lesson/sweepstakes/service/HorseMainRun.java
@@ -1,22 +1,42 @@
 package com.lesson.sweepstakes.service;
 
+import com.lesson.sweepstakes.dao.Horse;
 import com.lesson.sweepstakes.util.constant.HorseConstant;
-import lombok.SneakyThrows;
 
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class HorseMainRun {
-    @SneakyThrows
     public static void main(String[] args) {
-
         ExecutorService executorService = Executors.newFixedThreadPool(HorseConstant.HORSE_COUNT);
-        List<Future<Object>> sub = executorService.invokeAll();
-        for (Future<Object> f : sub) {
-            System.out.println("Hors param is:" + f.get());
+        List<Future<Horse>> resultList = null;
+        List<Race> raceList = new ArrayList<>();
+        for (int i = 0; i < HorseConstant.HORS_SIZE; i++) {
+            Race race = new Race();
+            raceList.add(race);
         }
-        System.out.println("FINISHED");
+
+        try {
+            resultList = executorService.invokeAll(raceList);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         executorService.shutdown();
+
+        for (int i = 0; i < resultList.size(); i++) {
+            Future<Horse> future = resultList.get(i);
+            try {
+                Horse horse = future.get();
+                System.out.println("Horse Id:"+horse.getId()+"\n"+"Horse speed:"+horse.getModelSpeed()+"\n"+"Horse race time:"+RaceRunGenerator.raceRun());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
--- JavaFxEEandOther/Java Project/sweepstakes/src/main/java/com/lesson/sweepstakes/service/Race.java
+++ JavaFxEEandOther/Java Project/sweepstakes/src/main/java/com/lesson/sweepstakes/service/Race.java
@@ -1,19 +1,20 @@
 package com.lesson.sweepstakes.service;
 
 import com.lesson.sweepstakes.dao.Horse;
+import com.lesson.sweepstakes.util.constant.HorseConstant;
+import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+@Getter
+public class Race implements Callable<Horse> {
 
-public class Race implements Callable<List<Object>>  {
+    public  static List<Horse>horsesParam(){
+        return HorseGenerator.generateHorses(HorseConstant.HORS_SIZE);
+    }
 
-    public static List<Horse> param() {return HorseGenerator.generateHorses(8);}
-
-    public  List<Object> call() throws Exception {
-        List<Object> horses = new ArrayList<>();
-        horses.add(RaceRunGenerator.raceRun(),param());
-        Thread.sleep(1000);
-        return horses;
+    @Override
+    public Horse call() {
+        return new Horse(HorseGenerator.generateHorse().getModelSpeed());
     }
 }
